91b20cdf63700a792873e5648536418a2b77f834
Thomas <thomas.blanc.2@etu.u-bordeaux.fr>
2016-03-16 15:07:13 +0100

    Ajout animatedGraphicElement <3 WAHOU DO A BARREL ROLL

---
+++ runner/animatedgraphicelement.cpp
@@ -0,0 +1,5 @@
+#include "animatedgraphicelement.h"
+
+AnimatedGraphicElement::AnimatedGraphicElement()
+{
+}
---
+++ runner/animatedgraphicelement.h
@@ -0,0 +1,10 @@
+#ifndef ANIMATEDGRAPHICELEMENT_H
+#define ANIMATEDGRAPHICELEMENT_H
+
+class AnimatedGraphicElement
+{
+public:
+    AnimatedGraphicElement();
+};
+
+#endif // ANIMATEDGRAPHICELEMENT_H
